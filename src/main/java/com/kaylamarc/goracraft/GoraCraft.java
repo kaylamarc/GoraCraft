279708ca38dacbc11e2a888428ec3ba06d967710
Kayla Marcantonio <kkcoons@aol.com>
2020-12-04 16:05:04 -0500

    Set up workspace

---
+++ src/main/java/com/kaylamarc/goracraft/GoraCraft.java
@@ -0,0 +1,30 @@
+package com.kaylamarc.goracraft;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("goracraft")
+public class GoraCraft
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public GoraCraft() {
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        // Register the doClientStuff method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) { }
+
+    private void doClientStuff(final FMLClientSetupEvent event) { }
+
+}
