4c248078274b00b8540a8bd476b38e31785ccb43
Kayla Marcantonio <kkcoons@aol.com>
2020-12-05 22:20:11 -0500

    Added ruby tools with crafting recipes

--- src/main/java/com/kaylamarc/goracraft/util/RegistryHandler.java
+++ src/main/java/com/kaylamarc/goracraft/util/RegistryHandler.java
@@ -5,8 +5,9 @@ import com.kaylamarc.goracraft.blocks.BlockItemBase;
 import com.kaylamarc.goracraft.blocks.RubyBlock;
 import com.kaylamarc.goracraft.blocks.RubyOreBlock;
 import com.kaylamarc.goracraft.items.ItemBase;
+import com.kaylamarc.goracraft.tools.ModItemTier;
 import net.minecraft.block.Block;
-import net.minecraft.item.Item;
+import net.minecraft.item.*;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -25,6 +26,18 @@ public class RegistryHandler {
 
     // Items
     public static final RegistryObject<Item> RUBY = ITEMS.register("ruby", ItemBase::new);
+
+    // Tools
+    public static final RegistryObject<SwordItem> RUBY_SWORD = ITEMS.register("ruby_sword", () ->
+            new SwordItem(ModItemTier.RUBY, 2, -1.7f, new Item.Properties().group(GoraCraft.TAB)));
+    public static final RegistryObject<PickaxeItem> RUBY_PICKAXE = ITEMS.register("ruby_pickaxe", () ->
+            new PickaxeItem(ModItemTier.RUBY, 0, -2.8f, new Item.Properties().group(GoraCraft.TAB)));
+    public static final RegistryObject<AxeItem> RUBY_AXE = ITEMS.register("ruby_axe", () ->
+            new AxeItem(ModItemTier.RUBY, 6, -3.1f, new Item.Properties().group(GoraCraft.TAB)));
+    public static final RegistryObject<ShovelItem> RUBY_SHOVEL = ITEMS.register("ruby_shovel", () ->
+            new ShovelItem(ModItemTier.RUBY, 0.6f, -3.0f, new Item.Properties().group(GoraCraft.TAB)));
+    public static final RegistryObject<HoeItem> RUBY_HOE = ITEMS.register("ruby_hoe", () ->
+            new HoeItem(ModItemTier.RUBY, -1.0f, new Item.Properties().group(GoraCraft.TAB)));
 
     // Blocks
     public static final RegistryObject<Block> RUBY_BLOCK = BLOCKS.register("ruby_block", RubyBlock::new);
