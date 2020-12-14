0622f7ccd6942a3e55857e1f65185110f930305e
Kayla Marcantonio <kkcoons@aol.com>
2020-12-13 23:27:38 -0500

    Implemented Raccoon taming

--- src/main/java/com/kaylamarc/goracraft/entities/RaccoonEntity.java
+++ src/main/java/com/kaylamarc/goracraft/entities/RaccoonEntity.java
@@ -1,56 +1,76 @@
 package com.kaylamarc.goracraft.entities;
 
-import com.kaylamarc.goracraft.GoraCraft;
 import com.kaylamarc.goracraft.init.ModEntityTypes;
 import com.kaylamarc.goracraft.init.ModItems;
-import net.minecraft.block.BlockState;
-import net.minecraft.entity.AgeableEntity;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.MobEntity;
+import net.minecraft.entity.*;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.ai.goal.*;
+import net.minecraft.entity.monster.AbstractSkeletonEntity;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.TameableEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.item.crafting.Ingredient;
-import net.minecraft.util.DamageSource;
-import net.minecraft.util.SoundEvent;
-import net.minecraft.util.SoundEvents;
-import net.minecraft.util.math.BlockPos;
+import net.minecraft.network.datasync.DataParameter;
+import net.minecraft.network.datasync.DataSerializers;
+import net.minecraft.network.datasync.EntityDataManager;
+import net.minecraft.util.*;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 
 import javax.annotation.Nullable;
+import java.util.UUID;
+import java.util.function.Predicate;
 
-public class RaccoonEntity extends TameableEntity {
+public class RaccoonEntity extends TameableEntity implements IAngerable {
 
     public static final Ingredient TEMPTATION_ITEMS = Ingredient.fromItems(ModItems.PIZZA.get(), Items.RABBIT_FOOT, Items.EGG, Items.SALMON, Items.COD, Items.SWEET_BERRIES);
+    private static final DataParameter<Integer> field_234232_bz_ = EntityDataManager.createKey(RaccoonEntity.class, DataSerializers.VARINT);
+    public static final Predicate<LivingEntity> TARGET_ENTITIES = (p_213440_0_) -> {
+        EntityType<?> entitytype = p_213440_0_.getType();
+        return entitytype == EntityType.RABBIT;
+    };
 
     public RaccoonEntity(EntityType<? extends TameableEntity> type, World worldIn) {
         super(type, worldIn);
+        this.setTamed(false);
     }
 
     public static AttributeModifierMap.MutableAttribute setCustomAttributes() {
         return MobEntity.func_233666_p_()   // registerAttributes()
                 .createMutableAttribute(Attributes.MAX_HEALTH, 9.0D)
                 .createMutableAttribute(Attributes.MOVEMENT_SPEED, 0.25D)
-                .createMutableAttribute(Attributes.FOLLOW_RANGE, 12.0D);
+                .createMutableAttribute(Attributes.FOLLOW_RANGE, 12.0D)
+                .createMutableAttribute(Attributes.ATTACK_DAMAGE, 5.0D);
     }
 
     @Override
     protected void registerGoals() {
         super.registerGoals();
-        this.goalSelector.addGoal(0, new FollowOwnerGoal(this, 1.2D, 2.0F, 10.0F, true));
+
+        // goals
         this.goalSelector.addGoal(1, new SwimGoal(this));
-        this.goalSelector.addGoal(2, new PanicGoal(this, 0.75D));
-        this.goalSelector.addGoal(3, new BreedGoal(this, 1.0D));
-        this.goalSelector.addGoal(4, new TemptGoal(this, 1.1D, TEMPTATION_ITEMS, true));
-        this.goalSelector.addGoal(5, new FollowParentGoal(this, 1.1D));
-        this.goalSelector.addGoal(6, new WaterAvoidingRandomWalkingGoal(this, 1.1D));
-        this.goalSelector.addGoal(7, new LookAtGoal(this, PlayerEntity.class, 6.0F));
-        this.goalSelector.addGoal(8, new LookRandomlyGoal(this));
+//        this.goalSelector.addGoal(2, new SitGoal(this));
+        this.goalSelector.addGoal(3, new LeapAtTargetGoal(this, 0.4F));
+        this.goalSelector.addGoal(4, new MeleeAttackGoal(this, 1.2D, true));
+        this.goalSelector.addGoal(5, new FollowOwnerGoal(this, 1.2D, 10.0F, 2.0F, false));
+        this.goalSelector.addGoal(6, new BreedGoal(this, 1.0D));
+        this.goalSelector.addGoal(8, new WaterAvoidingRandomWalkingGoal(this, 1.0D));
+//        this.goalSelector.addGoal(9, new BegGoal(this, 8.0F));
+        this.goalSelector.addGoal(9, new LookAtGoal(this, PlayerEntity.class, 8.0F));
+        this.goalSelector.addGoal(9, new LookRandomlyGoal(this));
+
+        // target
+        this.targetSelector.addGoal(1, new OwnerHurtByTargetGoal(this));
+        this.targetSelector.addGoal(2, new OwnerHurtTargetGoal(this));
+        this.targetSelector.addGoal(3, (new HurtByTargetGoal(this)).setCallsForHelp());
+        this.targetSelector.addGoal(4, new NearestAttackableTargetGoal<>(this, PlayerEntity.class, 10, true, false, this::func_233680_b_));
+        this.targetSelector.addGoal(5, new NonTamedTargetGoal<>(this, AnimalEntity.class, false, TARGET_ENTITIES));
+        this.targetSelector.addGoal(7, new NearestAttackableTargetGoal<>(this, AbstractSkeletonEntity.class, false));
+//        this.targetSelector.addGoal(8, new ResetAngerGoal<>(this, true));
     }
 
     /**
@@ -88,4 +108,102 @@ public class RaccoonEntity extends TameableEntity {
         return ModEntityTypes.RACCOON.get().create(this.world);
     }
 
+    @Override
+    public void setTamed(boolean tamed) {
+        super.setTamed(tamed);
+        if (tamed) {
+            this.getAttribute(Attributes.MAX_HEALTH).setBaseValue(20.0D);
+            this.setHealth(20.0F);
+        } else {
+            this.getAttribute(Attributes.MAX_HEALTH).setBaseValue(8.0D);
+        }
+
+        this.getAttribute(Attributes.ATTACK_DAMAGE).setBaseValue(4.0D);
+    }
+
+    // Handle taming
+    public ActionResultType func_230254_b_(PlayerEntity player, Hand hand) {
+        ItemStack itemstack = player.getHeldItem(hand);
+        Item item = itemstack.getItem();
+        if (this.world.isRemote) {
+            boolean flag = this.isOwner(player) || this.isTamed() || item == Items.CHICKEN || item == Items.COOKED_CHICKEN && !this.isTamed();
+            return flag ? ActionResultType.CONSUME : ActionResultType.PASS;
+        } else {
+            if (this.isTamed()) {
+                if (this.isBreedingItem(itemstack) && this.getHealth() < this.getMaxHealth()) {
+                    if (!player.abilities.isCreativeMode) {
+                        itemstack.shrink(1);
+                    }
+
+                    this.heal((float)item.getFood().getHealing());
+                    return ActionResultType.SUCCESS;
+                }
+
+//                THIS WILL BE IMPLEMENTED LATER (SITTING)
+//                ActionResultType actionresulttype = super.func_230254_b_(player, hand);
+//                if ((!actionresulttype.isSuccessOrConsume() || this.isChild()) && this.isOwner(player)) {
+//                    this.func_233687_w_(!this.isSitting());
+//                    this.isJumping = false;
+//                    this.navigator.clearPath();
+//                    this.setAttackTarget((LivingEntity)null);
+//                    return ActionResultType.SUCCESS;
+//                }
+//
+//                return actionresulttype;
+
+            } else if (item == Items.CHICKEN || item == Items.COOKED_CHICKEN) {
+                if (!player.abilities.isCreativeMode) {
+                    itemstack.shrink(1);
+                }
+
+                if (this.rand.nextInt(3) == 0 && !net.minecraftforge.event.ForgeEventFactory.onAnimalTame(this, player)) {
+                    this.setTamedBy(player);
+                    this.navigator.clearPath();
+                    this.setAttackTarget((LivingEntity)null);
+                    this.world.setEntityState(this, (byte)7);
+                } else {
+                    this.world.setEntityState(this, (byte)6);
+                }
+
+                return ActionResultType.SUCCESS;
+            }
+
+            return super.func_230254_b_(player, hand);
+        }
+    }
+
+    public static AttributeModifierMap.MutableAttribute func_234233_eS_() {
+        return MobEntity.func_233666_p_().createMutableAttribute(Attributes.MOVEMENT_SPEED, (double)0.3F).createMutableAttribute(Attributes.MAX_HEALTH, 8.0D).createMutableAttribute(Attributes.ATTACK_DAMAGE, 2.0D);
+    }
+
+    @Override
+    public int getAngerTime() {
+        return this.dataManager.get(field_234232_bz_);
+    }
+
+    @Override
+    public void setAngerTime(int time) {
+
+    }
+
+    @Nullable
+    @Override
+    public UUID getAngerTarget() {
+        return null;
+    }
+
+    @Override
+    public void setAngerTarget(@Nullable UUID target) {
+
+    }
+
+    @Override
+    public void func_230258_H__() {
+
+    }
+
+    @Override
+    public boolean isBreedingItem(ItemStack stack) {
+        return stack.getItem() == Items.CHICKEN || stack.getItem() == Items.COOKED_CHICKEN;
+    }
 }
